2efb7dd16cac4c21c85b61f1a12cb0c938ea15d8
yuvarajsingh-0 <yuvarajsingh170@gmail.com>
2023-08-06 22:58:26 +0530

    build resolving

--- src/sections/Donate.js
+++ src/sections/Donate.js
@@ -8,9 +8,9 @@ const Donate = () => {
                 <h3 className="donate-h3">HOW WE MAKE A DIFFERENCE <h1 className="gradient-txt donate-h1">Charity Matters</h1> </h3>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 <ul>
-                    <li className="donate-li"><img src='./check.png' className='check-img' />Funds go firectly to charity  </li>
-                    <li className="donate-li"><img src='./check.png' className='check-img' />Funds go firectly to charity  </li>
-                    <li className="donate-li"><img src='./check.png' className='check-img' />Funds go firectly to charity  </li>
+                    <li className="donate-li"><img src='./check.png' alt='' className='check-img' />Funds go firectly to charity  </li>
+                    <li className="donate-li"><img src='./check.png' alt='' className='check-img' />Funds go firectly to charity  </li>
+                    <li className="donate-li"><img src='./check.png' alt='' className='check-img' />Funds go firectly to charity  </li>
                 </ul>
             </div>
         </div>
